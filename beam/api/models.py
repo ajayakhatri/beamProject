ba594e221bf33144ef93d74ac92ad140d54b93c3
ajayakhatri <69041015+ajayakhatri@users.noreply.github.com>
2023-09-28 14:29:44 +0545

    Tidying up

--- beam/api/models.py
+++ beam/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 
 class BeamModel(models.Model):
     elasticity = models.FloatField(blank=True, null=True)
@@ -13,9 +11,3 @@ class BeamModel(models.Model):
 
     def __str__(self):
         return f"Beam reference no. {self.pk}: Length {self.length}{self.lengthunit}"
-
-
-#   calculation_data = models.JSONField()
-class MyModel(models.Model):
-    field1 = models.CharField(max_length=100)
-    field2 = models.TextField()
