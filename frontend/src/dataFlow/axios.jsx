f15b489d8f992cf782380552ca97b89b4b12341c
ajayakhatri <69041015+ajayakhatri@users.noreply.github.com>
2023-12-30 22:55:18 +0545

    Update axios.jsx

--- frontend/src/dataFlow/axios.jsx
+++ frontend/src/dataFlow/axios.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // we need to pass the baseURL as an object
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "http://127.0.0.1:8000/beam",
 });
 
 export default API;
